e10946887c0a86eadf6bc99f99f71ce859aeba68
Faruk Akyol <farukakyol441@gmail.com>
2018-03-12 19:44:33 +0300

    İlk commit.

---
+++ src/main/java/kurafloyka/EnglishVocabularyApplication/App.java
@@ -0,0 +1,13 @@
+package kurafloyka.EnglishVocabularyApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/kurafloyka/EnglishVocabularyApplication/Main.java
@@ -0,0 +1,11 @@
+package kurafloyka.EnglishVocabularyApplication;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ingilizce Kelime Uygulamasi");
+		System.out.println("deneme");
+	}
+
+}
