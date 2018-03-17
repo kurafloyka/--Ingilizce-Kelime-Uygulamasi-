a38a094a5f23ed990032087d976dd90751f958af
Faruk Akyol <farukakyol441@gmail.com>
2018-03-17 10:59:17 +0300

    kelimeEkleme fonksiyonu guncellendi.

--- src/main/java/kurafloyka/EnglishVocabularyApplication/Main2.java
+++ src/main/java/kurafloyka/EnglishVocabularyApplication/Main2.java
@@ -1,43 +1,19 @@
 package kurafloyka.EnglishVocabularyApplication;
 
-import java.util.Collections;
 import java.util.Iterator;
+import java.util.Scanner;
 import java.util.TreeSet;
 
 public class Main2 {
 
+	static TreeSet<Vocabulary2> v1;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		TreeSet<AnlamCumle> t = new TreeSet<AnlamCumle>(Collections.reverseOrder());
+		v1 = new TreeSet<Vocabulary2>();
 
-		t.add(new AnlamCumle("abc", "def"));
-		t.add(new AnlamCumle("araa", "ttt"));
-		t.add(new AnlamCumle("def", "sss"));
-		t.add(new AnlamCumle("abfc", "cumle"));
-
-		TreeSet<AnlamCumle> t2 = new TreeSet<AnlamCumle>(Collections.reverseOrder());
-
-		t2.add(new AnlamCumle("acsbc", "def"));
-		t2.add(new AnlamCumle("aaa", "ttt"));
-		t2.add(new AnlamCumle("deff", "sss"));
-		t2.add(new AnlamCumle("abck", "cumle"));
-
-		TreeSet<AnlamCumle> t3 = new TreeSet<AnlamCumle>(Collections.reverseOrder());
-
-		t3.add(new AnlamCumle("abc", "def"));
-		t3.add(new AnlamCumle("aaa", "ttt"));
-		t3.add(new AnlamCumle("def", "sss"));
-		t3.add(new AnlamCumle("abc", "cumle"));
-
-		System.out.println("--------------------------------------------");
-
-		TreeSet<Vocabulary2> v1 = new TreeSet<Vocabulary2>(Collections.reverseOrder());
-
-		v1.add(new Vocabulary2("faruk", t));
-		v1.add(new Vocabulary2("AHMET", t2));
-		v1.add(new Vocabulary2("elif", t3));
-		v1.add(new Vocabulary2("elif", t)); // kopyasini eklemez
+		menu();
 
 		Iterator<Vocabulary2> itr = v1.iterator();
 		while (itr.hasNext()) {
@@ -45,7 +21,91 @@ public class Main2 {
 			System.out.println(itr.next());
 
 		}
-
 	}
 
+	public static void menu() {
+
+		int tercih = 0;
+
+		while (true) {
+			System.out.println("***********************MENU*************************");
+			System.out.println("1. Tercih:Kelime Ekleme");
+			System.out.println("2. Tercih:Kelime Sorgulama");
+			System.out.println("3. Tercih:Kelime Silme");
+			System.out.println("4. Tercih:Kelime Listeleme");
+			System.out.println("5. Tercih:Kelime Guncelleme");
+			System.out.println("6. Tercih:Uygulamadan Cıkıs");
+			System.out.println("***********************MENU*************************");
+			System.out.println("                                                    ");
+			System.out.println("                                                    ");
+
+			System.out.println("Lutfen Tercihinizi Giriniz!!!");
+			Scanner oku1 = new Scanner(System.in);
+
+			tercih = oku1.nextInt();
+
+			switch (tercih) {
+			case 1:
+				kelimeEkleme();
+				break;
+
+			case 2:
+				// kelimeSorgulama();
+				break;
+			case 3:
+				// kelimeSilme();
+				break;
+
+			case 4:
+				// kelimeListeleme();
+				break;
+			case 5:
+				// kelimeGuncelleme();
+				break;
+
+			case 6:
+				return;
+
+			}
+
+		}
+	}
+
+	public static void kelimeEkleme() {
+
+		String kelime, anlam, cumle;
+		TreeSet<AnlamCumle> t3 = new TreeSet<AnlamCumle>();
+		Vocabulary2 kelimeAmlamlarCumleler;
+
+		Scanner oku = new Scanner(System.in);
+
+		System.out.println("Lutfen eklenecek kelimeyi giriniz");
+		kelime = oku.nextLine();
+		while (true) {
+
+			System.out.println("Lutfen eklenecek anlami ekleyiniz");
+			anlam = oku.nextLine();
+
+			System.out.println("Lutfen eklenecek cumleyi giriniz");
+			cumle = oku.nextLine();
+
+			t3.add(new AnlamCumle(anlam, cumle));
+
+			System.out.println(
+					kelime + " icin " + "Anlam olarak " + anlam + "----" + "Cumle olarak " + cumle + " eklendi....");
+
+			System.out.println("Baska anlam eklemek istiyor musunuz? evet/hayir");
+			String cevap;
+			cevap = oku.nextLine();
+
+			if (cevap.equals("hayir")) {
+				System.out.println("\"Kelime eklemeden cikildi...\"");
+				break;
+			}
+		}
+
+		kelimeAmlamlarCumleler = new Vocabulary2(kelime, t3);
+		v1.add(kelimeAmlamlarCumleler);
+
+	}
 }
